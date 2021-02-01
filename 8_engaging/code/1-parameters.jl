44d1c628a3d1f7e2585c8633a104437098624c8a
adelarue <adelarue@mit.edu>
2021-02-01 17:47:56 -0500

    Initial commit for Session 8 materials

---
+++ 8_engaging/code/1-parameters.jl
@@ -0,0 +1,23 @@
+using DataFrames, JLD2
+
+# Here we define the parameters we want to try (define the grid)
+# We will try every possible combination of these parameters
+depths = collect(2:2:20)
+maxfeatures = collect(2:2:10)
+numestimators = [20, 40, 80, 160]
+
+"""
+	This function is going to create a DataFrame,
+	where each row corresponds to the parameters for one experiment
+"""
+function getparameters(depths, maxfeatures, numestimators)
+	# let's code this up live!
+	
+end
+
+# Let's get these parameters into the right format
+
+# and now let's save them to a JLD file
+
+# Let's make a folder to save the results (if necessary)
+
---
+++ 8_engaging/code/4-plots.jl
@@ -0,0 +1,26 @@
+# Load the packages we need
+using JLD2, Plots, DataFrames
+
+# Load the parameters data frame
+
+
+# Create empty arrays that will hold the R2 values
+R2_train = []
+R2_val = []
+
+# Let's load all the results we want, and put them into the arrays
+for i = 1:nrow(params)
+	# load the results of experiments i
+
+end
+
+# Add the R2 values to the dataframe
+
+
+# Subset the dataframe to only some of the results
+
+
+# Make the plot
+
+
+Plots.savefig("plot.pdf")
